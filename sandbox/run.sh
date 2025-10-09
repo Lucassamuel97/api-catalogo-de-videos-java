56922506ebf3e0900404d93b75ed8d30bb0796d7
Lucassamuel97 <lukassamuka88@gmail.com>
2025-10-08 22:39:51 -0300

    :bricks: ci: rodando kafka com docker

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,11 +1,14 @@
 # Criar as docker networks
 docker network create elastic
+docker network create kafka
 
 # Criar os docker volumes
 docker volume create es01
+docker volume create kafka01
 
-
+# Criar as pastas com permissões
 docker compose -f elk/docker-compose.yml up -d elasticsearch
+docker compose -f kafka/docker-compose.yml up -d
 
 echo "Inicializando os containers..."
 sleep 20
